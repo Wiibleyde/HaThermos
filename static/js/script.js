9fdb46504ec84bc706d8d14d06500cfc9ba8f16b
Wiibleyde <nathan@bonnell.fr>
2023-04-20 11:39:31 +0200

    text size update
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,3 @@
-console.log("%cHATHERMOS WARNING: The JavaScript console is a powerful tool that can modify website behavior and expose sensitive information.", "color: red; font-size: 18px; font-weight: bold;");
-console.log("%cIf you are not a developer, you should not use this tool. If you are a developer, you should not use this tool on a website you do not own.", "color: red; font-size: 10px; font-weight: bold;");
-console.log("%cIf you are a developer and you are using this tool on a website you own, you should be careful what you do with it.", "color: red; font-size: 10px; font-weight: bold;");
+console.log("%cHATHERMOS WARNING: The JavaScript console is a powerful tool that can modify website behavior and expose sensitive information.", "color: red; font-size: 20px; font-weight: bold;");
+console.log("%cIf you are not a developer, you should not use this tool. If you are a developer, you should not use this tool on a website you do not own.", "color: red; font-size: 15px; font-weight: bold;");
+console.log("%cIf you are a developer and you are using this tool on a website you own, you should be careful what you do with it.", "color: red; font-size: 15px; font-weight: bold;");
