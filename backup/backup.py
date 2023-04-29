58781dc36fdd369165993cda5ec3dde4429999e8
Wiibleyde <nathan@bonnell.fr>
2023-04-29 20:04:46 +0200

    Fix backup
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- backup/backup.py
+++ backup/backup.py
@@ -1,12 +1,35 @@
-import pysftp
+import datetime
+import glob
+import logging
+import os
+import tarfile
+import time
 
-private_key_path = '.ssh/id_rsa'
+DATA_DIR = os.environ.get('DATA_DIR', '/app/data')
+MINECRAFT_DIR = os.environ.get('MINECRAFT_DIR', '/app/minecraft')
+BACKUP_DIR = os.environ.get('BACKUP_DIR', '/app/backup')
 
-with pysftp.Connection(private_key=private_key_path) as sftp:
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-    folder_path = '/var/etc/*'
-    backup_dir = '/temalepath'
-    
-    sftp.put_d(backup_dir, folder_path)
-    sftp.close()
-        
+def make_backup():
+    now = datetime.datetime.now()
+    date_time = now.strftime('%Y%m%d%H%M%S')
+    output_file = os.path.join(BACKUP_DIR, f'{date_time}.tar.gz')
+    files_to_backup = glob.glob(os.path.join(DATA_DIR, '*'))
+    files_to_backup.extend(glob.glob(os.path.join(MINECRAFT_DIR, '*')))
+    logger.info('Creating backup...')
+    with tarfile.open(output_file, 'w:gz') as tar:
+        for file in files_to_backup:
+            logger.info(f'Adding {file} to backup...')
+            tar.add(file)
+    logger.info(f'Backup created: {output_file}')
+
+if __name__ == '__main__':
+    while True:
+        try:
+            make_backup()
+        except Exception as e:
+            logger.exception('Backup failed', exc_info=e)
+        logger.info('Sleeping for 1 hour...')
+        time.sleep(3600)
